83c059ef78b78b6c7208e2ec7a8eebec98f32a7b
Sameer Srivastava <sameer.srivastava.17@mountblue.tech>
2021-10-20 11:31:23 +0530

    solved problem3

--- src/MainClass.java
+++ src/MainClass.java
@@ -8,12 +8,23 @@ import java.util.HashMap;
 
 public class MainClass {
     static HashMap<String,Integer> map=new HashMap<>();
+    static HashMap<String, Integer> winMap = new HashMap<>();
     public static void totalMatchesPlayedPerYear(String line){
         String[] value=line.split(",");
         if(map.containsKey(value[1])){
             map.put(value[1],map.get(value[1])+1);
         }else{
             map.put(value[1],1);
+        }
+    }
+    public static void matchesWonAllTeamAllYear(String value) {
+        String[] newValue = value.split(",");
+        if (newValue[10] != "") {
+            if (winMap.containsKey(newValue[10])) {
+                winMap.put(newValue[10], winMap.get(newValue[10]) + 1);
+            } else {
+                winMap.put(newValue[10], 1);
+            }
         }
     }
     public static void main(String[] args) throws FileNotFoundException {
@@ -29,6 +40,7 @@ public class MainClass {
             ArrayList<String> arrList=new ArrayList<>();
             while((matchesPerLine=br.readLine())!=null){
                 totalMatchesPlayedPerYear(matchesPerLine);
+                matchesWonAllTeamAllYear(matchesPerLine);
 
             }
 
@@ -38,6 +50,6 @@ public class MainClass {
             e.printStackTrace();
         }
        System.out.println(map);
-//        System.out.println(winMap);
+        System.out.println(winMap);
     }
 }
