cd46b4560e53baf9f09261a5b702fb483e56dc91
Sameer Srivastava <sameer.srivastava.17@mountblue.tech>
2021-10-20 15:11:06 +0530

    problem 3 solved

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,22 +1,29 @@
 
+
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class MainClass {
-    static HashMap<String,Integer> map=new HashMap<>();
+    static HashMap<String, Integer> map = new HashMap<>();
     static HashMap<String, Integer> winMap = new HashMap<>();
-    public static void totalMatchesPlayedPerYear(String line){
-        String[] value=line.split(",");
-        if(map.containsKey(value[1])){
-            map.put(value[1],map.get(value[1])+1);
-        }else{
-            map.put(value[1],1);
+    static Map<String,Integer> runs=new HashMap<>();
+
+    //static HashMap<String,HashMap<String,Integer>> map2=new HashMap<String,HashMap<String,Integer>>();
+    public static void totalMatchesPlayedPerYear(String line) {
+        String[] value = line.split(",");
+        if (map.containsKey(value[1])) {
+            map.put(value[1], map.get(value[1]) + 1);
+        } else {
+            map.put(value[1], 1);
         }
     }
+
     public static void matchesWonAllTeamAllYear(String value) {
         String[] newValue = value.split(",");
         if (newValue[10] != "") {
@@ -27,29 +34,58 @@ public class MainClass {
             }
         }
     }
-    public static void main(String[] args) throws FileNotFoundException {
-        String matchesPath="/home/sameer/IdeaProjects/Practice/src/com/company/matches.csv";
-        String deliveriesPath="/home/sameer/IdeaProjects/Practice/src/com/company/deliveries.csv";
-        String matchesPerLine="";
-        String deliveriesPerLine="";
-        try{
-            BufferedReader br=new BufferedReader(new FileReader(matchesPath));
-            BufferedReader in=new BufferedReader(new FileReader(deliveriesPath));
-            br.readLine();
-            in.readLine();
-            ArrayList<String> arrList=new ArrayList<>();
-            while((matchesPerLine=br.readLine())!=null){
-                totalMatchesPlayedPerYear(matchesPerLine);
-                matchesWonAllTeamAllYear(matchesPerLine);
 
+    public static void extraRunsConceded(ArrayList<String> list, ArrayList<String> deliveryData) {
+
+        String[]  listId=new String[list.size()];
+        list.toArray(listId);
+
+        for(int i = 0;i < deliveryData.size(); i++){
+            String[] arr = deliveryData.get(i).split(",");
+            //    int totalRuns=Integer.parseInt(arr[16]);
+            if(list.contains(arr[0])){
+                if(runs.containsKey(arr[3])){
+                    runs.put(arr[3],runs.get(arr[3])+Integer.parseInt(arr[16]));
+                }else{
+                    runs.put(arr[3],Integer.parseInt(arr[16]));
+                }
             }
 
-        } catch(FileNotFoundException e){
+        }
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String matchesPath = "/home/sameer/IdeaProjects/Practice/src/com/company/matches.csv";
+        String deliveriesPath = "/home/sameer/IdeaProjects/Practice/src/com/company/deliveries.csv";
+        String matchesPerLine = "";
+        String deliveriesPerLine = "";
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(matchesPath));
+            BufferedReader in = new BufferedReader(new FileReader(deliveriesPath));
+            br.readLine();
+            in.readLine();
+            ArrayList<String> matchesid = new ArrayList<>();
+            ArrayList<String> deliveryData=new ArrayList<>();
+            while ((matchesPerLine = br.readLine()) != null) {
+                totalMatchesPlayedPerYear(matchesPerLine);
+                matchesWonAllTeamAllYear(matchesPerLine);
+                if (matchesPerLine.split(",")[1].equals("2016")) {
+                    matchesid.add(matchesPerLine.split(",")[0]);
+                }
+
+            }
+            while ((deliveriesPerLine = in.readLine()) != null) {
+                deliveryData.add(deliveriesPerLine);
+            }
+            extraRunsConceded(matchesid,deliveryData);
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-       System.out.println(map);
+        System.out.println(map);
         System.out.println(winMap);
+        System.out.println(runs);
     }
 }
+
