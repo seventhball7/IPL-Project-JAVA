3361973bff85a75a7b269baf8bef36fed7d468a0
Sameer Srivastava <sameer.srivastava.17@mountblue.tech>
2021-10-20 10:38:19 +0530

    solved problem1

--- src/MainClass.java
+++ src/MainClass.java
@@ -7,6 +7,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MainClass {
+    static HashMap<String,Integer> map=new HashMap<>();
+    public static void totalMatchesPlayedPerYear(String line){
+        String[] value=line.split(",");
+        if(map.containsKey(value[1])){
+            map.put(value[1],map.get(value[1])+1);
+        }else{
+            map.put(value[1],1);
+        }
+    }
     public static void main(String[] args) throws FileNotFoundException {
         String matchesPath="/home/sameer/IdeaProjects/Practice/src/com/company/matches.csv";
         String deliveriesPath="/home/sameer/IdeaProjects/Practice/src/com/company/deliveries.csv";
@@ -19,20 +28,16 @@ public class MainClass {
             in.readLine();
             ArrayList<String> arrList=new ArrayList<>();
             while((matchesPerLine=br.readLine())!=null){
-//                totalMatchesPlayedPerYear(matchesPerLine);
-//                matchesWonAllTeamAllYear(matchesPerLine);
-//                if(matchesPerLine.split(",")[1].equals("2016")) {
-//                    arrList.add(matchesPerLine.split(",")[0]);
-//                }
-                System.out.println(matchesPerLine);
+                totalMatchesPlayedPerYear(matchesPerLine);
+
             }
-            System.out.println(arrList);
+
         } catch(FileNotFoundException e){
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-//        System.out.println(map);
+       System.out.println(map);
 //        System.out.println(winMap);
     }
 }
