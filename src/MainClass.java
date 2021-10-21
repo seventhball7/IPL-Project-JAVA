a44d3bbebbd3a74c3fcd2abef80e550a8e728821
Sameer Srivastava <sameer.srivastava.17@mountblue.tech>
2021-10-21 16:47:30 +0530

    solved problem4

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,19 +1,15 @@
 
-
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class MainClass {
     static HashMap<String, Integer> map = new HashMap<>();
     static HashMap<String, Integer> winMap = new HashMap<>();
     static Map<String,Integer> runs=new HashMap<>();
-
+    static Map<String,Float> economyBowlers=new HashMap<>();
     //static HashMap<String,HashMap<String,Integer>> map2=new HashMap<String,HashMap<String,Integer>>();
     public static void totalMatchesPlayedPerYear(String line) {
         String[] value = line.split(",");
@@ -53,7 +49,41 @@ public class MainClass {
 
         }
     }
+    static void economicaLBowlersof2015(List<String > list, List<String>deliveryData){
+        Map<String,Integer> bowlers=new HashMap<>();
+        Map<String,Integer> bowl=new HashMap<>();
+        Map<String,Integer> wideNo=new HashMap<>();
+        for(int i = 0;i < deliveryData.size(); i++) {
+            String[] arr = deliveryData.get(i).split(",");
+            if(list.contains(arr[0])) {
+                if (bowlers.containsKey(arr[8])){
+                    bowlers.put(arr[8], bowlers.get(arr[8]) + Integer.parseInt(arr[17]));
+                    bowl.put(arr[8],bowl.get(arr[8])+1);
+                }
+                else{
+                    bowlers.put(arr[8], Integer.parseInt(arr[17]));
+                    bowl.put(arr[8],1);
+                }
+                economyBowlers.put(arr[8],(bowlers.get(arr[8])*6f)/bowl.get(arr[8]));
+            }
+        }
+        Set<Map.Entry<String,Float>> entrySet=economyBowlers.entrySet();
+        List<Map.Entry<String,Float>> list1=new ArrayList<>(entrySet);
+        Collections.sort(list1, new Comparator<Map.Entry<String, Float>>() {
+            @Override
+            public int compare(Map.Entry<String, Float> o1, Map.Entry<String, Float> o2) {
+                return o1.getValue().compareTo(o2.getValue());
+            }
+        });
+        HashMap<String,Float> eBowler=new HashMap<>();
+        int c=0;
+        list1.forEach(s->{
+            eBowler.put(s.getKey(),s.getValue());
+        });
+//         System.out.println(eBowler);
+        System.out.println(list1);
 
+    }
     public static void main(String[] args) throws FileNotFoundException {
         String matchesPath = "/home/sameer/IdeaProjects/Practice/src/com/company/matches.csv";
         String deliveriesPath = "/home/sameer/IdeaProjects/Practice/src/com/company/deliveries.csv";
@@ -65,6 +95,7 @@ public class MainClass {
             br.readLine();
             in.readLine();
             ArrayList<String> matchesid = new ArrayList<>();
+            ArrayList<String> matchesidof2015 = new ArrayList<>();
             ArrayList<String> deliveryData=new ArrayList<>();
             while ((matchesPerLine = br.readLine()) != null) {
                 totalMatchesPlayedPerYear(matchesPerLine);
@@ -72,12 +103,16 @@ public class MainClass {
                 if (matchesPerLine.split(",")[1].equals("2016")) {
                     matchesid.add(matchesPerLine.split(",")[0]);
                 }
+                if (matchesPerLine.split(",")[1].equals("2015")) {
+                    matchesidof2015.add(matchesPerLine.split(",")[0]);
+                }
 
             }
             while ((deliveriesPerLine = in.readLine()) != null) {
                 deliveryData.add(deliveriesPerLine);
             }
             extraRunsConceded(matchesid,deliveryData);
+            economicaLBowlersof2015(matchesidof2015,deliveryData);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
