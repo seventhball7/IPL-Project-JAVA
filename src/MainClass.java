243459523c70818eda2b4906fb891948a7f64e93
Sameer Srivastava <sameer.srivastava.17@mountblue.tech>
2021-10-23 01:27:29 +0530

    minor changes to the file

--- src/MainClass.java
+++ src/MainClass.java
@@ -6,29 +6,33 @@ import java.io.IOException;
 import java.util.*;
 
 public class MainClass {
-    static HashMap<String, Integer> map = new HashMap<>();
-    static HashMap<String, Integer> winMap = new HashMap<>();
-    static Map<String, Integer> runs = new HashMap<>();
-    static Map<String, Float> economyBowlers = new HashMap<>();
+    static HashMap<String, Integer> totalMatchesPerYear = new HashMap<>();
+    static HashMap<String, Integer> matchesWonAllTeamAllYear = new HashMap<>();
+    static Map<String, Integer> extraRunsin2016 = new HashMap<>();
+    static Map<String, Float> economyBowlersin2015 = new HashMap<>();
+
 
     public static void totalMatchesPlayedPerYear(String line) {
         String[] value = line.split(",");
-        if (map.containsKey(value[1])) {
-            map.put(value[1], map.get(value[1]) + 1);
+        if (totalMatchesPerYear.containsKey(value[1])) {
+            totalMatchesPerYear.put(value[1], totalMatchesPerYear.get(value[1]) + 1);
         } else {
-            map.put(value[1], 1);
+            totalMatchesPerYear.put(value[1], 1);
         }
     }
+
+
     public static void matchesWonAllTeamAllYear(String value) {
         String[] newValue = value.split(",");
         if (newValue[10] != "") {
-            if (winMap.containsKey(newValue[10])) {
-                winMap.put(newValue[10], winMap.get(newValue[10]) + 1);
+            if (matchesWonAllTeamAllYear.containsKey(newValue[10])) {
+                matchesWonAllTeamAllYear.put(newValue[10], matchesWonAllTeamAllYear.get(newValue[10]) + 1);
             } else {
-                winMap.put(newValue[10], 1);
+                matchesWonAllTeamAllYear.put(newValue[10], 1);
             }
         }
     }
+
 
     public static void extraRunsConceded(ArrayList<String> list, ArrayList<String> deliveryData) {
         String[] listId = new String[list.size()];
@@ -36,52 +40,51 @@ public class MainClass {
         for (int i = 0; i < deliveryData.size(); i++) {
             String[] arr = deliveryData.get(i).split(",");
             if (list.contains(arr[0])) {
-                if (runs.containsKey(arr[3])) {
-                    runs.put(arr[3], runs.get(arr[3]) + Integer.parseInt(arr[16]));
+                if (extraRunsin2016.containsKey(arr[3])) {
+                    extraRunsin2016.put(arr[3], extraRunsin2016.get(arr[3]) + Integer.parseInt(arr[16]));
                 } else {
-                    runs.put(arr[3], Integer.parseInt(arr[16]));
+                    extraRunsin2016.put(arr[3], Integer.parseInt(arr[16]));
                 }
             }
         }
     }
 
-    static void economicaLBowlersof2015(List<String> list, List<String> deliveryData) {
-        Map<String, Integer> bowlers = new HashMap<>();
-        Map<String, Integer> bowl = new HashMap<>();
-//        Map<String, Integer> wideNo = new HashMap<>();
+
+    static void economicaLBowlersof2015(List<String> listIdof2015, List<String> deliveryData) {
+        Map<String, Integer> bowlersWithRuns = new HashMap<>();
+        Map<String, Integer> bowlersWithBowl = new HashMap<>();
         for (int i = 0; i < deliveryData.size(); i++) {
             String[] arr = deliveryData.get(i).split(",");
-            if (list.contains(arr[0])) {
-                if (bowlers.containsKey(arr[8])) {
-                    bowlers.put(arr[8], bowlers.get(arr[8]) + Integer.parseInt(arr[17]));
-                    bowl.put(arr[8], bowl.get(arr[8]) + 1);
+            if (listIdof2015.contains(arr[0])) {
+                if (bowlersWithRuns.containsKey(arr[8])) {
+                    bowlersWithRuns.put(arr[8], bowlersWithRuns.get(arr[8]) + Integer.parseInt(arr[17]));
+                    bowlersWithBowl.put(arr[8], bowlersWithBowl.get(arr[8]) + 1);
                 } else {
-                    bowlers.put(arr[8], Integer.parseInt(arr[17]));
-                    bowl.put(arr[8], 1);
+                    bowlersWithRuns.put(arr[8], Integer.parseInt(arr[17]));
+                    bowlersWithBowl.put(arr[8], 1);
                 }
-                economyBowlers.put(arr[8], (bowlers.get(arr[8]) * 6f) / bowl.get(arr[8]));
+                economyBowlersin2015.put(arr[8], (bowlersWithRuns.get(arr[8]) * 6f) / bowlersWithBowl.get(arr[8]));
             }
         }
-        Set<Map.Entry<String, Float>> entrySet = economyBowlers.entrySet();
-        List<Map.Entry<String, Float>> list1 = new ArrayList<>(entrySet);
-        Collections.sort(list1, new Comparator<Map.Entry<String, Float>>() {
+        Set<Map.Entry<String, Float>> entrySet = economyBowlersin2015.entrySet();
+        List<Map.Entry<String, Float>> sortingEconomyBowlers = new ArrayList<>(entrySet);
+        Collections.sort(sortingEconomyBowlers, new Comparator<Map.Entry<String, Float>>() {
             @Override
             public int compare(Map.Entry<String, Float> o1, Map.Entry<String, Float> o2) {
                 return o1.getValue().compareTo(o2.getValue());
             }
         });
-        HashMap<String, Float> eBowler = new HashMap<>();
-        int c = 0;
-        list1.forEach(s -> {
-            eBowler.put(s.getKey(), s.getValue());
-        });
-        System.out.println("4. For the year 2015 the top economical bowlers \n"+list1);
+//        HashMap<String, Float> eBowler = new HashMap<>();
+//        sortingEconomyBowlers.forEach(s -> {
+//            eBowler.put(s.getKey(), s.getValue());
+//        });
+        System.out.println("4. For the year 2015 the top economical bowlers \n" + sortingEconomyBowlers);
     }
 
 
     public static void main(String[] args) throws FileNotFoundException {
-        String matchesPath = "/home/sameer/IdeaProjects/Practice/src/com/company/matches.csv";
-        String deliveriesPath = "/home/sameer/IdeaProjects/Practice/src/com/company/deliveries.csv";
+        String matchesPath = "/home/sameer/IdeaProjects/IPL-JAVA/src/matches.csv";
+        String deliveriesPath = "/home/sameer/IdeaProjects/IPL-JAVA/src/deliveries.csv";
         String matchesPerLine = "";
         String deliveriesPerLine = "";
         try {
@@ -89,14 +92,14 @@ public class MainClass {
             BufferedReader in = new BufferedReader(new FileReader(deliveriesPath));
             br.readLine();
             in.readLine();
-            ArrayList<String> matchesid = new ArrayList<>();
+            ArrayList<String> matchesidOf2016 = new ArrayList<>();
             ArrayList<String> matchesidof2015 = new ArrayList<>();
             ArrayList<String> deliveryData = new ArrayList<>();
             while ((matchesPerLine = br.readLine()) != null) {
                 totalMatchesPlayedPerYear(matchesPerLine);
                 matchesWonAllTeamAllYear(matchesPerLine);
                 if (matchesPerLine.split(",")[1].equals("2016")) {
-                    matchesid.add(matchesPerLine.split(",")[0]);
+                    matchesidOf2016.add(matchesPerLine.split(",")[0]);
                 }
                 if (matchesPerLine.split(",")[1].equals("2015")) {
                     matchesidof2015.add(matchesPerLine.split(",")[0]);
@@ -105,16 +108,15 @@ public class MainClass {
             while ((deliveriesPerLine = in.readLine()) != null) {
                 deliveryData.add(deliveriesPerLine);
             }
-            extraRunsConceded(matchesid, deliveryData);
+            extraRunsConceded(matchesidOf2016, deliveryData);
             economicaLBowlersof2015(matchesidof2015, deliveryData);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("1. matches played per year of all the years in IPL: \n" +map);
-        System.out.println("2. Number of matches won of all teams over all the years of IPL: \n"+winMap);
-        System.out.println("3. For the year 2016 the extra runs conceded per team: \n"+runs);
+        System.out.println("1. matches played per year of all the years in IPL: \n" + totalMatchesPerYear);
+        System.out.println("2. Number of matches won of all teams over all the years of IPL: \n" + matchesWonAllTeamAllYear);
+        System.out.println("3. For the year 2016 the extra runs conceded per team: \n" + extraRunsin2016);
     }
 }
-
